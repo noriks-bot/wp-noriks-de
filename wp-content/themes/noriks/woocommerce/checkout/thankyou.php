614c8e2c2615519095102ceaec63eed5e523eeb2
Noriks (via Claude) <info@noriks.com>
2026-07-27 07:50:11 +0200

    Thankyou upsell: prave 3x/5x SIVE kompozitne slike (generirane iz produktne, isti stil kot crne) v temi (img/upsell), URL prek get_template_directory_uri

--- wp-content/themes/noriks/woocommerce/checkout/thankyou.php
+++ wp-content/themes/noriks/woocommerce/checkout/thankyou.php
@@ -57,8 +57,8 @@ if ( $upsell_is_tshirts ) {
     $upsell_qty_names  = array( 1 => '1x Graue Boxershorts', 3 => '3x Graue Boxershorts', 5 => '5x Graue Boxershorts' );
     $upsell_qty_images = array(
         1 => 'https://noriks.com/de/wp-content/uploads/2025/11/sive-boksarice-produktna.jpg',
-        3 => 'https://noriks.com/de/wp-content/uploads/2025/11/sive-boksarice-produktna.jpg', // TODO: 3x siva kompozitna, ko obstaja
-        5 => 'https://noriks.com/de/wp-content/uploads/2025/11/sive-boksarice-produktna.jpg', // TODO: 5x siva kompozitna, ko obstaja
+        3 => get_template_directory_uri() . '/img/upsell/siva-3x.png',
+        5 => get_template_directory_uri() . '/img/upsell/siva-5x.png',
     );
     $upsell_title_text = 'Jetzt Boxershorts hinzufuegen - 50 % Rabatt';
 }
