71d2565974164945c51492e76e7ee9b3d94eff12
Noriks (via Claude) <info@noriks.com>
2026-08-31 07:49:09 +0200

    Blagajna: nezno preverjanje telefonske stevilke (ustavi le crke, prekratko ali absurdno dolgo) + tiha pretvorba v mednarodno obliko

--- wp-content/themes/noriks/functions.php
+++ wp-content/themes/noriks/functions.php
@@ -9,6 +9,7 @@ include(get_template_directory() . '/functions/product-type.php');
 include(get_template_directory() . '/functions/pack-switcher.php'); // Auswahl der Paketgroesse + andere Farbkombinationen (Xer-Sets)
 include(get_template_directory() . '/functions/flash-deals-banner.php'); // traka sezonske rasprodaje
 include(get_template_directory() . '/functions/checkout_mods.php');
+include(get_template_directory() . '/functions/phone-validate.php'); // nezno preverjanje telefonske stevilke
 include(get_template_directory() . '/functions/thankyou_upsell.php');
 include(get_template_directory() . '/functions/product-page-upsell.php'); // upsell okvir ispod gumba na stranici proizvoda (ACF prekidac)
 include(get_template_directory() . '/functions/cpts.php');
